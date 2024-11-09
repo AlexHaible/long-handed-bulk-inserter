0a9fb362e103cbbbc44b50413af877da2ce93303
Alex Haible <52239823+AlexHaible@users.noreply.github.com>
2024-11-09 20:43:32 +0100

    Add weight attribute to inserter
    This makes it so a rocket can carry 50, instead of 42

--- prototypes/items.lua
+++ prototypes/items.lua
@@ -4,5 +4,6 @@ longHandedBulkInserterItem.name = "long-handed-bulk-inserter"
 longHandedBulkInserterItem.icon = SauravisusSprite("icons/long-handed-bulk-inserter")
 longHandedBulkInserterItem.place_result = "long-handed-bulk-inserter"
 longHandedBulkInserterItem.order = "g[long-handed-bulk-inserter]"
+longHandedBulkInserterItem.weight = 1000000/50
 
 data:extend{longHandedBulkInserterItem}
